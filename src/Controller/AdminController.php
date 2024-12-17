52d6f8cf4f4c869787e92d3f2254aff1e0c7f828
salma <seghaiersalma@gmail.com>
2024-12-18 00:26:21 +0100

    updates new

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\AbonnementRepository;
+use App\Repository\CourseRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +12,18 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'admin_dashboard')]
-    public function dashboard(): Response
+    public function dashboard(UserRepository $userRepo , CourseRepository $coursRepo, AbonnementRepository $abonnementRepo): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        $totalUsers = count($userRepo->findAll());
+        $totalSubscriptions = count($abonnementRepo->findAll());
+        $totalCourses = count($coursRepo->findAll());
+        $newRegistrations = count($userRepo->findBy(['roles' => 'ROLE_USER']));
+        return $this->render('admin/dashboard.html.twig', [
+            'totalUsers' => $totalUsers,
+            'totalSubscriptions' => $totalSubscriptions,
+            'totalCourses' => $totalCourses,
+            'newRegistrations' => $newRegistrations,
+        ]);
     }
 
 }
